032db498fd75a061e533388e94b1ff252a8028c1
Guma <65368237+PujaPai@users.noreply.github.com>
2024-10-25 18:15:27 +0200

    Add files via upload

--- 2Dgame/main.py
+++ 2Dgame/main.py
@@ -3,38 +3,138 @@ from pygame.locals import *
 
 pygame.init()
 
+clock = pygame.time.Clock()
+fps = 75
+
 screen_width = 800
 screen_height = 800
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('2D Gra')
 
-
+#wielkość platform
 tile_size = 50
 
 
-#ładowanie spraitów
+#ładowanie tła
 sun_img = pygame.image.load('2Dgame/img/sun.png')
 bg_img = pygame.image.load('2Dgame/img/cave.png')
 
-def draw_grid():
+
+#rysowanie siatki na całej mapie
+"""def draw_grid():
     for line in range(0, 16):
         pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))"""
 
 class Player():
     def __init__(self, x, y):
-        img = pygame.image.load('2Dgame/img/gracz1.png')
-        self.image = pygame.transform.scale(img, (40, 80))
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+        
+        #wybieranie spritów gracza
+        for num in range(1, 5):
+            img_right = pygame.image.load(f'2Dgame/img/player{num}.png')
+            img_right = pygame.transform.scale(img_right, (34, 50))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
+        self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.vel_y = 0
+        self.jumped = False
+        self.direction = 0
+
+    def update(self):
+        dx = 0
+        dy = 0
+        walk_cooldown = 3
+
+        #Znajdywanie czy przycisk jest kliknięty
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            dx -= 5
+            self.counter += 1
+            self.direction = -1
+        if key[pygame.K_RIGHT]:
+            dx += 5
+            self.counter += 1
+            self.direction = 1
+        if key[pygame.K_SPACE] and self.jumped == False:
+            self.vel_y = -15
+            self.jumped = True
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
+        if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+            self.counter = 0
+            self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+
+
+        #animacje
+        if self.counter > walk_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images_right):
+                self.index = 1
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+
+
+        #grawitacja
+        self.vel_y += 1
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
+        
+        
+        #Sprawdzanie kolizji
+        for tile in world.tile_list:
+            #X
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                dx = 0
+            #Y
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                #Sprawdzanie skoku
+                if self.vel_y < 0:
+                    dy = tile[1].bottom - self.rect.top
+                    self.vel_y = 0
+                #Sprawdzanie spadku
+                elif self.vel_y >= 0:
+                    dy = tile[1].top - self.rect.bottom
+                    self.vel_y = 0
+
+                
+
+
+        #Zmienianie położenia postaci
+        self.rect.x += dx
+        self.rect.y += dy
+        
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+            dy = 0
+
+        #rysowanie gracza
+        screen.blit(self.image, self.rect)
+        pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
 
 class World():
     def __init__(self, data):
         self.tile_list = []
 
-        #ładowanie spraitów
+        #ładowanie terenu
         stone_img = pygame.image.load('2Dgame/img/stone.png')
         moss_img = pygame.image.load('2Dgame/img/moss.png')
 
@@ -56,13 +156,36 @@ class World():
                     img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
+                if tile == 3:
+                    """jak by sprite był mniejszy dodaj cyfry"""
+                    slime = Enemy(col_count * tile_size, row_count * tile_size)
+                    enemy_group.add(slime)
                 col_count += 1
             row_count += 1
 
     def draw(self):
         for tile in self.tile_list:
             screen.blit(tile[0], tile[1])
+            pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
 
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('2Dgame/img/slime.png')
+        self.rect = self.image.get_rect()
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+        self.move_counter = 0
+
+    def update(self):
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+        if self.move_counter > 50:
+            self.move_direction *= -1
+            self.move_counter *= -1
 
 world_data = [
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -77,7 +200,7 @@ world_data = [
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 1],
 [1, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 1, 1, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 1, 1, 1, 0, 1],
 [1, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 1, 1, 1, 0, 1],
 [1, 0, 0, 0, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 0, 1],
 [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -85,21 +208,27 @@ world_data = [
 
 
 
+player = Player(100, screen_height - 100)
+
+enemy_group = pygame.sprite.Group()
 
 world = World(world_data)
-gracz = Player(100, screen_height - 130)
-
 
 
 run = True
 while run == True:
-
+    clock.tick(fps)
     screen.blit(pygame.transform.scale(bg_img, (800, 800)), (0, 0))
     screen.blit(pygame.transform.scale(sun_img, (100, 100)), (100, 100))
 
     world.draw()
 
-    draw_grid()
+    enemy_group.update()
+    enemy_group.draw(screen)
+
+    player.update()
+
+    """draw_grid()"""
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
